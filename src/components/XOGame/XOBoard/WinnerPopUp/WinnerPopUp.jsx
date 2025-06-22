4ebe7ee62cf24e525764f9195070342c9eb80a76
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-23 02:04:01 +0300

    feat(sounds): add victory sound on player win and tie sound on draw

--- src/components/XOGame/XOBoard/WinnerPopUp/WinnerPopUp.jsx
+++ src/components/XOGame/XOBoard/WinnerPopUp/WinnerPopUp.jsx
@@ -1,17 +1,38 @@
 "use client";
 
 import { SYMBOL_O, SYMBOL_X } from "@/data/constants";
+import {
+  DRAW_SOUND_EFFECT,
+  soundFiles,
+  WINNER_SOUND_EFFECT,
+} from "@/data/sounds";
+import usePreloadSounds from "@/hooks/usePreloadSounds";
 import { useXOStore } from "@/stores/xo.store/xo.store";
 import s from "./WinnerPopUp.module.scss";
 
 const WinnerPopUp = () => {
   const { winner, isWinnerPopupVisible } = useXOStore((s) => s);
+  const playSound = usePreloadSounds(soundFiles);
+
+  const isDraw = winner === "Draw!";
+  const isP1Win = winner === SYMBOL_O;
+  const isP2Win = winner === SYMBOL_X;
+
+  if (isWinnerPopupVisible) {
+    if (isDraw) {
+      playSound(DRAW_SOUND_EFFECT, 0.1);
+    }
+
+    if (isP1Win || isP2Win) {
+      playSound(WINNER_SOUND_EFFECT, 0.1);
+    }
+  }
 
   const classes = [
     s.winner,
-    winner === "Draw!" ? s.draw : "",
-    winner === SYMBOL_O ? s.p1 : "",
-    winner === SYMBOL_X ? s.p2 : "",
+    isDraw ? s.draw : "",
+    isP1Win ? s.p1 : "",
+    isP2Win ? s.p2 : "",
     isWinnerPopupVisible ? s.show : "",
   ].join(" ");
 
--- src/data/sounds.js
+++ src/data/sounds.js
@@ -10,6 +10,13 @@ export const soundFiles = {
   swap1: "/sounds/swap-1.mp3",
   swap2: "/sounds/swap-2.mp3",
   unselect: "/sounds/unselect.mp3",
+  longVictory: "/sounds/long-victory.mp3",
+  victory2: "/sounds/victory-2.mp3",
+  victory3: "/sounds/victory-3.mp3",
+  victory4: "/sounds/victory-4.mp3",
+  victoryOrTie: "/sounds/victory-or-tie.mp3",
+  draw: "/sounds/tie.mp3",
+  missionFailed: "/sounds/mission-failed.mp3",
 };
 
 export const BUTTON_SOUND = "click4";
@@ -17,3 +24,5 @@ export const UNSELECT_SOUND = "unselect";
 export const FREEZE_SOUND = "freeze";
 export const BOMB_SOUND = "bomb1";
 export const SWAP_SYMBOLS_SOUND = "swap1";
+export const WINNER_SOUND_EFFECT = "victory2";
+export const DRAW_SOUND_EFFECT = "victoryOrTie";
