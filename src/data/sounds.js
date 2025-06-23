c8790561c65a2b1cec4353b2e3bc784f6c16b3f3
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-24 01:27:53 +0300

    chore(sounds): rename sound files and reorder sound path definitions

--- src/data/sounds.js
+++ src/data/sounds.js
@@ -1,21 +1,21 @@
 export const soundFiles = {
+  click4: "/sounds/click-4.mp3",
+  unselect: "/sounds/unselect.mp3",
+  freeze: "/sounds/freeze-1.mp3",
   bomb1: "/sounds/bomb-1.mp3",
   bomb2: "/sounds/bomb-2.mp3",
-  click4: "/sounds/click-4.mp3",
-  freeze: "/sounds/freeze-1.mp3",
   swap1: "/sounds/swap-1.mp3",
   swap2: "/sounds/swap-2.mp3",
-  unselect: "/sounds/unselect.mp3",
+  victory1: "/sounds/victory-1.mp3",
   victory2: "/sounds/victory-2.mp3",
-  victory4: "/sounds/victory-4.mp3",
-  victoryOrTie: "/sounds/victory-or-tie.mp3",
+  draw: "/sounds/draw.mp3",
 };
 
 export const BUTTON_SOUND = "click4";
 export const UNSELECT_SOUND = "unselect";
 export const FREEZE_SOUND = "freeze";
 export const SWAP_SYMBOLS_SOUND = "swap1";
-export const DRAW_SOUND = "victoryOrTie";
+export const DRAW_SOUND = "draw";
 
 // [soundName, soundVolume]
 export const BOMB_SOUNDS = [
