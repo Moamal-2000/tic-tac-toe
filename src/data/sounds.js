304d09e8283ce2cdaed57b4d1142389e16c0362b
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-24 01:24:52 +0300

    remove(sounds): remove unused sound files

--- src/data/sounds.js
+++ src/data/sounds.js
@@ -1,22 +1,14 @@
 export const soundFiles = {
   bomb1: "/sounds/bomb-1.mp3",
   bomb2: "/sounds/bomb-2.mp3",
-  click1: "/sounds/click-1.mp3",
-  click2: "/sounds/click-2.mp3",
-  click3: "/sounds/click-3.mp3",
   click4: "/sounds/click-4.mp3",
-  click5: "/sounds/click-5.mp3",
   freeze: "/sounds/freeze-1.mp3",
   swap1: "/sounds/swap-1.mp3",
   swap2: "/sounds/swap-2.mp3",
   unselect: "/sounds/unselect.mp3",
-  longVictory: "/sounds/long-victory.mp3",
   victory2: "/sounds/victory-2.mp3",
-  victory3: "/sounds/victory-3.mp3",
   victory4: "/sounds/victory-4.mp3",
   victoryOrTie: "/sounds/victory-or-tie.mp3",
-  draw: "/sounds/tie.mp3",
-  missionFailed: "/sounds/mission-failed.mp3",
 };
 
 export const BUTTON_SOUND = "click4";
