0aeb3c678feb6ee6b879e8ed5cfe4186f46d5d3d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-30 10:13:43 +0300

    add(MultiPlayerMenu): add jsx stricture

--- src/components/MainMenu/MultiPlayerMenu/MultiPlayerMenu.jsx
+++ src/components/MainMenu/MultiPlayerMenu/MultiPlayerMenu.jsx
@@ -1,7 +1,39 @@
+import { BOARD_SIZES } from "@/data/constants";
 import s from "./MultiPlayerMenu.module.scss";
 
 const MultiPlayerMenu = () => {
-  return <div className={s.multiplayerContent}></div>;
+  return (
+    <div className={s.mpContent}>
+      <header className={s.header}>
+        <h2>Multiplayer Setup</h2>
+        <p>Configure your game settings</p>
+      </header>
+
+      <form className={s.mpForm}>
+        <div className={s.boardSelectionWrapper}>
+          <label>Board Size</label>
+
+          <div className={s.boardSelection}>
+            {BOARD_SIZES.map((size) => (
+              <div className={s.option} key={`boardSize-${size}`}>
+                <input
+                  type="radio"
+                  name="boardSize"
+                  id={`boardSize-${size}`}
+                  value={size}
+                />
+                <label htmlFor={`boardSize-${size}`}>
+                  {size}x{size}
+                </label>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <button type="submit">Join & Wait</button>
+      </form>
+    </div>
+  );
 };
 
 export default MultiPlayerMenu;
