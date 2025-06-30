ff597ec6ab054bc0d9ee38f2ba8cb2aeb4698afc
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-30 10:05:24 +0300

    init: MultiPlayerMenu component

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -3,6 +3,7 @@
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import s from "./MainMenu.module.scss";
 import MainMenuButtons from "./MainMenuButtons/MainMenuButtons";
+import MultiPlayerMenu from "./MultiPlayerMenu/MultiPlayerMenu";
 
 const MainMenu = () => {
   const { isMainMenuActive, gameMode } = useGlobalStore((s) => s);
@@ -13,6 +14,7 @@ const MainMenu = () => {
     <div className={s.menuOverlay}>
       <section className={s.mainMenu}>
         {gameMode !== "multiplayer" && <MainMenuButtons />}
+        {gameMode === "multiplayer" && <MultiPlayerMenu />}
       </section>
     </div>
   );
---
+++ src/components/MainMenu/MultiPlayerMenu/MultiPlayerMenu.jsx
@@ -0,0 +1,7 @@
+import s from "./MultiPlayerMenu.module.scss";
+
+const MultiPlayerMenu = () => {
+  return <div className={s.multiplayerContent}></div>;
+};
+
+export default MultiPlayerMenu;
