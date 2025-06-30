6cf382b3eea3fac2416f00720007065ac070b312
Moamal-2000 <moamalalaapro1@gmail.com>
2025-06-30 09:49:29 +0300

    feat(MainMenu): close menu on single-player mode

--- src/components/MainMenu/MainMenu.jsx
+++ src/components/MainMenu/MainMenu.jsx
@@ -1,16 +1,26 @@
 "use client";
 
 import { GAME_MODES_BUTTONS } from "@/data/staticData";
+import { useGlobalStore } from "@/stores/global.store/global.store";
 import SvgIcon from "../Shared/SvgIcon";
 import s from "./MainMenu.module.scss";
 
 const MainMenu = () => {
+  const { updateGameMode, isMainMenuActive } = useGlobalStore((s) => s);
+
+  if (!isMainMenuActive) return null;
+
   return (
     <div className={s.menuOverlay}>
       <section className={s.mainMenu}>
         <div className={s.buttons}>
           {GAME_MODES_BUTTONS.map(({ label, iconName, id }) => (
-            <button type="button" className={s.button} key={`${label}-${id}`}>
+            <button
+              type="button"
+              className={s.button}
+              key={`${label}-${id}`}
+              onClick={() => updateGameMode(label)}
+            >
               <div className={s.iconHolder}>
                 <SvgIcon name={iconName} />
               </div>
--- src/stores/global.store/global.store.js
+++ src/stores/global.store/global.store.js
@@ -2,11 +2,13 @@ import { create } from "zustand";
 
 export const useGlobalStore = create((set, get) => ({
   isAboutModelActive: false,
-  isMainMenuActive: false,
+  isMainMenuActive: true,
   gameMode: null, // "singleplayer" | "multiplayer"
 
   toggleAboutModel: (value) => {
     set({ isAboutModelActive: value ? value : !get().isAboutModelActive });
   },
-
+  updateGameMode: (gameMode) => {
+    set({ gameMode, isMainMenuActive: gameMode !== "singleplayer" });
+  },
 }));
