3f3ab29468771c2bb0c84ae4dd718da533629249
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-02 12:15:39 +0300

    feat(XOGame): render MultiPlayerUI component based on 'multiplayer' game mode

--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,12 +1,18 @@
 "use client";
 
 import { useGlobalStore } from "@/stores/global.store/global.store";
+import MultiPlayerUI from "./MultiPlayerUI/MultiPlayerUI";
 import SinglePlayerUI from "./SinglePlayerUI/SinglePlayerUI";
 
 const XOGame = () => {
   const { gameMode } = useGlobalStore((s) => s);
 
-  return gameMode === "singleplayer" && <SinglePlayerUI />;
+  return (
+    <>
+      {gameMode === "singleplayer" && <SinglePlayerUI />}
+      {gameMode === "multiplayer" && <MultiPlayerUI />}
+    </>
+  );
 };
 
 export default XOGame;
