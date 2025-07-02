62fbced5eb90365dbde93a01a4bf9c87b8358808
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-02 12:14:20 +0300

    feat(XOGame): move single player UI into separate SinglePlayerUI component for 'singleplayer' mode

--- src/components/XOGame/SinglePlayerUI/SinglePlayerUI.jsx
+++ src/components/XOGame/SinglePlayerUI/SinglePlayerUI.jsx
@@ -1,9 +1,26 @@
-import s from './SinglePlayerUI.module.scss'
+import { useXOStore } from "@/stores/xo.store/xo.store";
+import GameStats from "./GameStats/GameStats";
+import PlayerTurnIndicator from "./PlayerTurnIndicator/PlayerTurnIndicator";
+import PowerUps from "./PowerUps/PowerUps";
+import s from "./SinglePlayerUI.module.scss";
+import XOBoard from "./XOBoard/XOBoard";
 
 const SinglePlayerUI = () => {
-  return (
-    <div>SinglePlayerUI</div>
-  )
-}
+  const boardSize = useXOStore((s) => s.boardSize);
+  const board3Class = boardSize === 3 ? s.x3 : "";
 
-export default SinglePlayerUI
+  return (
+    <section className={`${s.game} ${board3Class}`}>
+      <div className={s.wrapper}>
+        <PowerUps player="player1" />
+        <GameStats />
+        <PowerUps player="player2" />
+      </div>
+
+      <XOBoard />
+      <PlayerTurnIndicator />
+    </section>
+  );
+};
+
+export default SinglePlayerUI;
--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,28 +1,12 @@
 "use client";
 
-import { useXOStore } from "@/stores/xo.store/xo.store";
-import GameStats from "./GameStats/GameStats";
-import PlayerTurnIndicator from "./PlayerTurnIndicator/PlayerTurnIndicator";
-import PowerUps from "./PowerUps/PowerUps";
-import XOBoard from "./XOBoard/XOBoard";
-import s from "./XOGame.module.scss";
+import { useGlobalStore } from "@/stores/global.store/global.store";
+import SinglePlayerUI from "./SinglePlayerUI/SinglePlayerUI";
 
 const XOGame = () => {
-  const boardSize = useXOStore((s) => s.boardSize);
-  const board3Class = boardSize === 3 ? s.x3 : "";
+  const { gameMode } = useGlobalStore((s) => s);
 
-  return (
-    <section className={`${s.game} ${board3Class}`}>
-      <div className={s.wrapper}>
-        <PowerUps player="player1" />
-        <GameStats />
-        <PowerUps player="player2" />
-      </div>
-
-      <XOBoard />
-      <PlayerTurnIndicator />
-    </section>
-  );
+  return gameMode === "singleplayer" && <SinglePlayerUI />;
 };
 
 export default XOGame;
