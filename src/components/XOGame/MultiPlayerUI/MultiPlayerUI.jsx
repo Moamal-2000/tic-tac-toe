8b2df074a9049676fbce9585d587bd4fcc6cfba4
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-02 12:09:14 +0300

    init: SinglePlayerUI and MultiPlayerUI components

---
+++ src/components/XOGame/MultiPlayerUI/MultiPlayerUI.jsx
@@ -0,0 +1,9 @@
+import s from './MultiPlayerUI.module.scss'
+
+const MultiPlayerUI = () => {
+  return (
+    <div>MultiPlayerUI</div>
+  )
+}
+
+export default MultiPlayerUI
---
+++ src/components/XOGame/SinglePlayerUI/SinglePlayerUI.jsx
@@ -0,0 +1,9 @@
+import s from './SinglePlayerUI.module.scss'
+
+const SinglePlayerUI = () => {
+  return (
+    <div>SinglePlayerUI</div>
+  )
+}
+
+export default SinglePlayerUI
