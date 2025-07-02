52b98cf95d425a9cb136aad315fadc73561c6444
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-02 12:18:02 +0300

    feat(BoardSelector): prevent rendering component when mode is 'multiplayer'

--- src/components/Header/HeaderButtons/BoardSelector/BoardSelector.jsx
+++ src/components/Header/HeaderButtons/BoardSelector/BoardSelector.jsx
@@ -1,11 +1,15 @@
 "use client";
 
 import { BOARD_SIZES } from "@/data/constants";
+import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useXOStore } from "@/stores/xo.store/xo.store";
 import s from "./BoardSelector.module.scss";
 
 const BoardSelector = ({ playClickSound }) => {
   const { boardSize, updateBoardSize } = useXOStore((s) => s);
+  const gameMode = useGlobalStore((s) => s.gameMode);
+
+  if (gameMode === "multiplayer") return null;
 
   function handleClick(size) {
     updateBoardSize(size);
--- src/components/XOGame/MultiPlayerUI/MultiPlayerUI.jsx
+++ src/components/XOGame/MultiPlayerUI/MultiPlayerUI.jsx
@@ -2,7 +2,7 @@ import s from './MultiPlayerUI.module.scss'
 
 const MultiPlayerUI = () => {
   return (
-    <div>MultiPlayerUI</div>
+    <div></div>
   )
 }
 
