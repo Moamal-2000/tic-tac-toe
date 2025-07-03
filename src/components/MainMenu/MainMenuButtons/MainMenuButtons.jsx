58ab69a002a3c092a0f0a221c1d81c8739b00f0f
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-03 12:31:01 +0300

    feat(MainMenuButtons): add click sound on buttons

--- src/components/MainMenu/MainMenuButtons/MainMenuButtons.jsx
+++ src/components/MainMenu/MainMenuButtons/MainMenuButtons.jsx
@@ -1,10 +1,18 @@
 import SvgIcon from "@/components/Shared/SvgIcon";
+import { BUTTON_SOUND, soundFiles } from "@/data/sounds";
 import { GAME_MODES_BUTTONS } from "@/data/staticData";
+import usePreloadSounds from "@/hooks/usePreloadSounds";
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import s from "./MainMenuButtons.module.scss";
 
 const MainMenuButtons = () => {
   const updateGameMode = useGlobalStore((s) => s.updateGameMode);
+  const playSound = usePreloadSounds({ click: soundFiles.click });
+
+  function handleClick(label) {
+    updateGameMode(label);
+    playSound(BUTTON_SOUND);
+  }
 
   return (
     <div className={s.buttons}>
@@ -13,7 +21,7 @@ const MainMenuButtons = () => {
           type="button"
           className={s.button}
           key={`${label}-${id}`}
-          onClick={() => updateGameMode(label)}
+          onClick={() => handleClick(label)}
         >
           <SvgIcon name={iconName} />
           {label}
