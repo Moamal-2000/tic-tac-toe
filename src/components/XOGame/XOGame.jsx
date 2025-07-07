36ef35256ca3b74a40722411bf3c02dce18c4d9e
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-07 13:06:22 +0300

    fix(XOGame): show MultiPlayerUI only when hasGameStarted is true

--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,16 +1,18 @@
 "use client";
 
 import { useGlobalStore } from "@/stores/global.store/global.store";
+import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import MultiPlayerUI from "./MultiPlayerUI/MultiPlayerUI";
 import SinglePlayerUI from "./SinglePlayerUI/SinglePlayerUI";
 
 const XOGame = () => {
-  const { gameMode } = useGlobalStore((s) => s);
+  const gameMode = useGlobalStore((s) => s.gameMode);
+  const hasGameStarted = useMultiplayerStore((s) => s.hasGameStarted);
 
   return (
     <>
       {gameMode === "local" && <SinglePlayerUI />}
-      {gameMode === "online" && <MultiPlayerUI />}
+      {gameMode === "online" && hasGameStarted && <MultiPlayerUI />}
     </>
   );
 };
