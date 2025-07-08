dc0278e975b9057978cab055c81430b0856e56da
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-08 17:57:36 +0300

    test(iconsData): add preserveAspectRatio attribute with "none" value

--- src/data/iconsData.jsx
+++ src/data/iconsData.jsx
@@ -26,7 +26,14 @@ export const iconsData = [
   {
     name: "x-symbol",
     icon: (
-      <svg width="26" stroke="#4ecdc4" strokeWidth="8" viewBox="0 0 100 99">
+      <svg
+        width="26"
+        height="26"
+        stroke="#4ecdc4"
+        strokeWidth="8"
+        viewBox="0 0 100 99"
+        preserveAspectRatio="none"
+      >
         <path d="m20 20 60 60M80 20 20 80" />
       </svg>
     ),
@@ -34,7 +41,14 @@ export const iconsData = [
   {
     name: "o-symbol",
     icon: (
-      <svg width="20" stroke="#ff6b6b" strokeWidth="8" viewBox="0 0 100 96">
+      <svg
+        width="20"
+        height="20"
+        stroke="#ff6b6b"
+        strokeWidth="8"
+        viewBox="0 0 100 96"
+        preserveAspectRatio="none"
+      >
         <circle cx="50" cy="50" r="40" fill="none" />
       </svg>
     ),
