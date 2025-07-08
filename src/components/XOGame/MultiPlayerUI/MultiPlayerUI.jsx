1209637959330876b76b806d8678c9f3ec8f2226
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-08 13:38:53 +0300

    feat(MultiPlayerUI): display game states board

--- src/components/XOGame/MultiPlayerUI/MultiPlayerUI.jsx
+++ src/components/XOGame/MultiPlayerUI/MultiPlayerUI.jsx
@@ -1,14 +1,21 @@
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
+import GameStats from "../GameStats/GameStats";
 import PlayerTurnIndicator from "../PlayerTurnIndicator/PlayerTurnIndicator";
 import s from "./MultiPlayerUI.module.scss";
 import XOBoard from "./XOBoard/XOBoard";
 
 const MultiPlayerUI = () => {
-  const { boardSize, winner, playerTurn } = useMultiplayerStore((s) => s);
+  const { boardSize, stats, winner, playerTurn } = useMultiplayerStore(
+    (s) => s
+  );
   const board3Class = boardSize === 3 ? s.x3 : "";
 
   return (
     <section className={`${s.game} ${board3Class}`}>
+      <div className={s.wrapper}>
+        <GameStats boardSize={boardSize} stats={stats} />
+      </div>
+
       <XOBoard />
       <PlayerTurnIndicator
         boardSize={boardSize}
--- src/stores/multiplayer.store/multiplayer.store.js
+++ src/stores/multiplayer.store/multiplayer.store.js
@@ -10,6 +10,7 @@ export const useMultiplayerStore = create((set, get) => ({
   draw: false,
   isWinnerPopupVisible: false,
   selectedBoardSize: INITIAL_BOARD_SIZE,
+  stats: { p1Wins: 0, draws: 0, p2Wins: 0 },
 
   updateGameStates: ({
     boardSize,
