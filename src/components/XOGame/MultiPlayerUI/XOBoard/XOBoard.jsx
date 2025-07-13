5b4e7ada540dc3aa0f8f72a020e0a9a6b78693f6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-13 22:30:47 +0300

    fix(XOBoard): highlight board in yellow on draw state for online mode

--- src/components/XOGame/MultiPlayerUI/XOBoard/XOBoard.jsx
+++ src/components/XOGame/MultiPlayerUI/XOBoard/XOBoard.jsx
@@ -5,9 +5,9 @@ import WinnerPopUp from "./WinnerPopUp/WinnerPopUp";
 import s from "./XOBoard.module.scss";
 
 const XOBoard = () => {
-  const { board, playerTurn, winner } = useMultiplayerStore((s) => s);
+  const { board, playerTurn, draw } = useMultiplayerStore((s) => s);
   const playerTurnClass = playerTurn === SYMBOL_X ? s.xTurn : s.oTurn;
-  const drawClass = winner === "Draw!" ? s.draw : "";
+  const drawClass = draw ? s.draw : "";
 
   return (
     <div className={`${s.board} ${playerTurnClass} ${drawClass}`}>
