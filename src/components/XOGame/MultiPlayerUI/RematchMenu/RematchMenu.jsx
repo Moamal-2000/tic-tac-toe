036f8e169ed04df64852efa2a200517763856dd6
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-14 14:51:22 +0300

    style(RematchMenu): add overlay and basic styles to the menu

--- src/components/XOGame/MultiPlayerUI/RematchMenu/RematchMenu.jsx
+++ src/components/XOGame/MultiPlayerUI/RematchMenu/RematchMenu.jsx
@@ -2,12 +2,14 @@ import s from "./RematchMenu.module.scss";
 
 const RematchMenu = () => {
   return (
-    <div className={s.rematchMenu}>
-      <p>Ask for a rematch?</p>
+    <div className={s.overlay}>
+      <div className={s.rematchMenu}>
+        <p>Ask for a rematch?</p>
 
-      <div className={s.buttons}>
-        <button className={s.yes}>Yes</button>
-        <button className={s.no}>No</button>
+        <div className={s.buttons}>
+          <button className={s.yes}>Yes</button>
+          <button className={s.no}>No</button>
+        </div>
       </div>
     </div>
   );
