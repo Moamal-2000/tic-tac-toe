0373fcdbf74415d5313ba06e1097fc489431c1e2
Moamal-2000 <moamalalaapro1@gmail.com>
2025-07-14 14:54:47 +0300

    feat(RematchMenu): close the menu when clicking overlay or selecting 'No'

--- src/components/XOGame/MultiPlayerUI/RematchMenu/RematchMenu.jsx
+++ src/components/XOGame/MultiPlayerUI/RematchMenu/RematchMenu.jsx
@@ -1,14 +1,25 @@
+"use client";
+
+import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import s from "./RematchMenu.module.scss";
 
 const RematchMenu = () => {
+  const { updateMultiplayerState } = useMultiplayerStore((s) => s);
+
+  function closeRematchMenu() {
+    updateMultiplayerState({ isRematchMenuActive: false });
+  }
+
   return (
-    <div className={s.overlay}>
+    <div className={s.overlay} onClick={closeRematchMenu}>
       <div className={s.rematchMenu}>
         <p>Ask for a rematch?</p>
 
         <div className={s.buttons}>
           <button className={s.yes}>Yes</button>
-          <button className={s.no}>No</button>
+          <button className={s.no} onClick={closeRematchMenu}>
+            No
+          </button>
         </div>
       </div>
     </div>
