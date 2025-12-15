65baf7bdfb39967a1d72e1a5ae3d314731b44d4d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-15 03:53:02 +0300

    feat(socket): connect front-end to Replit back-end for online gameplay

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,3 +1,3 @@
 import { io } from "socket.io-client";
 
-export const socket = io.connect("http://localhost:4000");
+export const socket = io.connect("https://71a9d769-e312-42d7-adc4-b03086da406a-00-2jybze9r5kci8.kirk.replit.dev");
