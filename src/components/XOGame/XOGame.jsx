ca8246ba82f228e1672e9061e2da563e0456d59d
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-22 05:12:51 +0300

    debug(XOGame): add console log to debug env values

--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { BASE_URL, IS_PRODUCTION, SOCKET_SERVER_URL } from "@/data/env";
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import MultiPlayerUI from "./MultiPlayerUI/MultiPlayerUI";
@@ -8,6 +9,8 @@ import SinglePlayerUI from "./SinglePlayerUI/SinglePlayerUI";
 const XOGame = () => {
   const gameMode = useGlobalStore((s) => s.gameMode);
   const hasGameStarted = useMultiplayerStore((s) => s.hasGameStarted);
+
+  console.log("Testing", { IS_PRODUCTION, BASE_URL, SOCKET_SERVER_URL });
 
   return (
     <>
