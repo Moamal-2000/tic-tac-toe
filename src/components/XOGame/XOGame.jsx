89ac49d0c75b6ad0c804c06c3fd662abe20fa916
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-22 05:25:31 +0300

    debug(XOGame): remove console log of debugging env values

--- src/components/XOGame/XOGame.jsx
+++ src/components/XOGame/XOGame.jsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { BASE_URL, IS_PRODUCTION, SOCKET_SERVER_URL } from "@/data/env";
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import MultiPlayerUI from "./MultiPlayerUI/MultiPlayerUI";
@@ -9,8 +8,6 @@ import SinglePlayerUI from "./SinglePlayerUI/SinglePlayerUI";
 const XOGame = () => {
   const gameMode = useGlobalStore((s) => s.gameMode);
   const hasGameStarted = useMultiplayerStore((s) => s.hasGameStarted);
-
-  console.log("Testing", { IS_PRODUCTION, BASE_URL, SOCKET_SERVER_URL });
 
   return (
     <>
