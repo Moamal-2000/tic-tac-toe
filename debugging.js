b506a9b03293bb3b43b0be601b2a8df154d6ca20
Moamal-2000 <moamalalaapro1@gmail.com>
2025-12-22 04:47:44 +0300

    remove(debug): remove debugging.js file for local socket testing

--- debugging.js
+++
@@ -1,9 +0,0 @@
-import { socket } from "@/socket/socket";
-
-export function debugging(globalStore) {
-  const { updateGameMode, updateGlobalState } = globalStore;
-
-  updateGameMode("online");
-  socket.emit("matchmaking", 3);
-  updateGlobalState({ isWaitingForOpponent: false });
-}
