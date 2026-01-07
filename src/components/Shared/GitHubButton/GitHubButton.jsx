106d2ace830bcf9eed2bd472f200c6b2da30db0f
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-08 02:07:37 +0300

    feat(intl): implement multi-language support for GitHub button title

--- src/components/Shared/GitHubButton/GitHubButton.jsx
+++ src/components/Shared/GitHubButton/GitHubButton.jsx
@@ -3,9 +3,11 @@
 import { GITHUB_URL } from "@/data/links";
 import { BUTTON_SOUND, soundFiles } from "@/data/sounds";
 import usePreloadSounds from "@/hooks/usePreloadSounds";
+import { useTranslations } from "next-intl";
 import s from "./GitHubButton.module.scss";
 
 const GitHubButton = () => {
+  const t = useTranslations("header");
   const playSound = usePreloadSounds({ click: soundFiles.click });
 
   return (
@@ -14,7 +16,7 @@ const GitHubButton = () => {
       target="_blank"
       className={s.button}
       rel="noreferrer"
-      title="Visit GitHub Repository"
+      title={t("github")}
       onClick={() => playSound(BUTTON_SOUND)}
     >
       <svg aria-hidden="true">
