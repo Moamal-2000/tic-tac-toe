9a54e92e1a0728bdf0aa2204c97af1a31df58c2f
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-09 17:12:51 +0300

    fix(GameStats): use the correct store for singleplayer and multiplayer modes

--- src/components/XOGame/GameStats/GameStats.jsx
+++ src/components/XOGame/GameStats/GameStats.jsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { SCREEN_SIZES } from "@/data/constants";
+import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
+import { useXOStore } from "@/stores/xo.store/xo.store";
 import { useTranslations } from "next-intl";
 import PlayerTurnIndicator from "../PlayerTurnIndicator/PlayerTurnIndicator";
 import s from "./GameStats.module.scss";
 
 const GameStats = ({ stats }) => {
-  const { boardSize, playerTurn, winner } = useMultiplayerStore();
   const t = useTranslations("game_stats");
+  const gameMode = useGlobalStore((s) => s.gameMode);
+  const isSinglePlayerMode = gameMode === "local" || gameMode === "computer";
+
+  const { boardSize, playerTurn, winner } = isSinglePlayerMode
+    ? useXOStore()
+    : useMultiplayerStore();
 
   const board3Class = boardSize === 3 ? s.x3 : "";
 
