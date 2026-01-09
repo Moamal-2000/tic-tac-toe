e07b6739be322ea0f9c93b6b8b16c1638c82683b
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-09 18:02:04 +0300

    refactor(HeaderButtons): extract language selector into LanguageSelector component

--- src/components/Header/HeaderButtons/HeaderButtons.jsx
+++ src/components/Header/HeaderButtons/HeaderButtons.jsx
@@ -4,36 +4,25 @@ import InstallPWAButton from "@/components/PWA/InstallPWAButton";
 import Button from "@/components/Shared/Button/Button";
 import { BUTTON_SOUND, soundFiles } from "@/data/sounds";
 import usePreloadSounds from "@/hooks/usePreloadSounds";
-import { languagesMenu } from "@/i18n/config";
-import { usePathname, useRouter } from "@/i18n/navigation";
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import { useXOStore } from "@/stores/xo.store/xo.store";
 import { useTranslations } from "next-intl";
-import Image from "next/image";
 import BoardSelector from "./BoardSelector/BoardSelector";
 import FullscreenToggleButton from "./FullscreenToggleButton/FullscreenToggleButton";
 import s from "./HeaderButtons.module.scss";
+import LanguageSelector from "./LanguageSelector/LanguageSelector";
 import PlayModeSelector from "./PlayModeSelector/PlayModeSelector";
 import VolumeButton from "./VolumeButton/VolumeButton";
 
 const HeaderButtons = () => {
   const t = useTranslations("header");
   const resetStats = useXOStore((s) => s.resetStats);
-  const {
-    toggleAboutModel,
-    gameMode,
-    updateGameMode,
-    isLangMenuActive,
-    updateGlobalState,
-  } = useGlobalStore();
+  const { toggleAboutModel, gameMode, updateGameMode } = useGlobalStore();
   const { updateMultiplayerState } = useMultiplayerStore((s) => s);
 
   const playSound = usePreloadSounds({ click: soundFiles.click });
   const isOnlineMode = gameMode === "online";
-
-  const pathname = usePathname();
-  const router = useRouter();
 
   function handleAboutClick() {
     toggleAboutModel();
@@ -53,17 +42,6 @@ const HeaderButtons = () => {
     playSound(BUTTON_SOUND);
   }
 
-  function handleToggleLangMenu({ isOpen } = {}) {
-    updateGlobalState({
-      isLangMenuActive: isOpen !== undefined ? isOpen : !isLangMenuActive,
-    });
-  }
-
-  function handleLangClick(countryCode) {
-    router.replace(pathname, { locale: countryCode });
-    handleToggleLangMenu({ isOpen: false });
-  }
-
   return (
     <div className={`${s.headerButtons} ${isOnlineMode ? s.onlineMode : ""}`}>
       <div className={s.wrapper1}>
@@ -77,31 +55,7 @@ const HeaderButtons = () => {
         />
         <VolumeButton />
         <InstallPWAButton playClickSound={() => playSound(BUTTON_SOUND)} />
-        <div className={s.languageSection}>
-          <Button onClick={handleToggleLangMenu}>
-            {t("language")}
-            <svg aria-hidden="true">
-              <use href="/icons-sprite.svg#chevronDown"></use>
-            </svg>
-          </Button>
-
-          <div
-            className={`${s.languageSelect} ${
-              isLangMenuActive ? s.active : ""
-            }`}
-          >
-            {languagesMenu.map(({ code, name, flag, alt }) => (
-              <button
-                key={code}
-                type="button"
-                onClick={() => handleLangClick(code)}
-              >
-                {name}
-                <Image src={flag} alt={alt} width={20} height={12} />
-              </button>
-            ))}
-          </div>
-        </div>
+        <LanguageSelector />
         <Button onClick={handleAboutClick}>{t("about")}</Button>
         <Button onClick={handleMenuClick}>{t("menu")}</Button>
         <Button onClick={handleResetClick}>
--- src/components/Header/HeaderButtons/LanguageSelector/LanguageSelector.jsx
+++ src/components/Header/HeaderButtons/LanguageSelector/LanguageSelector.jsx
@@ -1,8 +1,55 @@
-import s from './LanguageSelector.module.scss'
+"use client";
+
+import Button from "@/components/Shared/Button/Button";
+import { languagesMenu } from "@/i18n/config";
+import { usePathname, useRouter } from "@/i18n/navigation";
+import { useGlobalStore } from "@/stores/global.store/global.store";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import s from "./LanguageSelector.module.scss";
 
 const LanguageSelector = () => {
+  const t = useTranslations("header");
+  const { updateGlobalState, isLangMenuActive } = useGlobalStore();
+
+  const router = useRouter();
+  const pathname = usePathname();
+
+  function handleToggleLangMenu({ isOpen } = {}) {
+    updateGlobalState({
+      isLangMenuActive: isOpen !== undefined ? isOpen : !isLangMenuActive,
+    });
+  }
+
+  function handleLangClick(countryCode) {
+    router.replace(pathname, { locale: countryCode });
+    handleToggleLangMenu({ isOpen: false });
+  }
+
   return (
-    <div>LanguageSelector</div>
-  )
-}
-export default LanguageSelector
+    <div className={s.languageSection}>
+      <Button onClick={handleToggleLangMenu}>
+        {t("language")}
+        <svg aria-hidden="true">
+          <use href="/icons-sprite.svg#chevronDown"></use>
+        </svg>
+      </Button>
+
+      <div
+        className={`${s.languageSelect} ${isLangMenuActive ? s.active : ""}`}
+      >
+        {languagesMenu.map(({ code, name, flag, alt }) => (
+          <button
+            key={code}
+            type="button"
+            onClick={() => handleLangClick(code)}
+          >
+            {name}
+            <Image src={flag} alt={alt} width={20} height={12} />
+          </button>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default LanguageSelector;
