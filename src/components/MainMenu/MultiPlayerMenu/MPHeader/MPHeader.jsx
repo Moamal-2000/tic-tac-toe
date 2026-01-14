534e01def4a9dc5fb27eefb4519ae6ca731f6c89
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-14 19:55:54 +0300

    refactor(MultiPlayerMenu): extract header structure into MPHeader component

--- src/components/MainMenu/MultiPlayerMenu/MPHeader/MPHeader.jsx
+++ src/components/MainMenu/MultiPlayerMenu/MPHeader/MPHeader.jsx
@@ -1,8 +1,42 @@
-import s from "./MPHeader.module.scss"
+import { socket } from "@/socket/socket";
+import { useTranslations } from "next-intl";
+import { useEffect, useState } from "react";
+import s from "./MPHeader.module.scss";
 
 const MPHeader = () => {
+  const t = useTranslations("main_menu");
+
+  const [onlinePlayers, setOnlinePlayers] = useState(0);
+
+  useEffect(() => {
+    socket.emit("get-online-players");
+
+    function handleOnlinePlayersCount({ count }) {
+      setOnlinePlayers(count);
+    }
+
+    socket.on("online-players-count", handleOnlinePlayersCount);
+
+    return () => {
+      socket.off("online-players-count", handleOnlinePlayersCount);
+    };
+  }, []);
+
   return (
-    <header></header>
-  )
-}
-export default MPHeader
+    <header className={s.header}>
+      <div className={s.playerCount}>
+        <span className={s.playerCountDot} />
+        <span className={s.playerCountLabel}>
+          {t("multiplayer.setup.online_players")}
+        </span>
+        <span className={s.playerCountValue}>{onlinePlayers}</span>
+      </div>
+
+      <div className={s.headerText}>
+        <h1>{t("multiplayer.setup.title")}</h1>
+        <p>{t("multiplayer.setup.subtitle")}</p>
+      </div>
+    </header>
+  );
+};
+export default MPHeader;
--- src/components/MainMenu/MultiPlayerMenu/MultiPlayerMenu.jsx
+++ src/components/MainMenu/MultiPlayerMenu/MultiPlayerMenu.jsx
@@ -7,8 +7,8 @@ import { socket } from "@/socket/socket";
 import { useGlobalStore } from "@/stores/global.store/global.store";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
 import { useTranslations } from "next-intl";
-import { useEffect, useState } from "react";
 import MPBoardSelection from "./MPBoardSelection/MPBoardSelection";
+import MPHeader from "./MPHeader/MPHeader";
 import s from "./MultiPlayerMenu.module.scss";
 
 const MultiPlayerMenu = () => {
@@ -16,7 +16,6 @@ const MultiPlayerMenu = () => {
 
   const { updateGameMode, updateGlobalState } = useGlobalStore();
   const selectedBoardSize = useMultiplayerStore((s) => s.selectedBoardSize);
-  const [onlinePlayers, setOnlinePlayers] = useState(0);
 
   const playSound = usePreloadSounds({ click: soundFiles.click });
 
@@ -32,39 +31,10 @@ const MultiPlayerMenu = () => {
     updateGameMode(null);
   }
 
-  useEffect(() => {
-    // Request initial count
-    socket.emit("get-online-players");
-
-    const handleOnlinePlayersCount = ({ count }) => {
-      setOnlinePlayers(count);
-    };
-
-    socket.on("online-players-count", handleOnlinePlayersCount);
-
-    return () => {
-      socket.off("online-players-count", handleOnlinePlayersCount);
-    };
-  }, []);
-
   return (
     <div className={s.mpContent}>
       <BackButton onClick={handleBackButton} />
-
-      <header className={s.header}>
-        <div className={s.playerCount}>
-          <span className={s.playerCountDot} />
-          <span className={s.playerCountLabel}>
-            {t("multiplayer.setup.online_players")}
-          </span>
-          <span className={s.playerCountValue}>{onlinePlayers}</span>
-        </div>
-
-        <div className={s.headerText}>
-          <h1>{t("multiplayer.setup.title")}</h1>
-          <p>{t("multiplayer.setup.subtitle")}</p>
-        </div>
-      </header>
+      <MPHeader />
 
       <form className={s.mpForm} onSubmit={handleSubmit}>
         <MPBoardSelection />
