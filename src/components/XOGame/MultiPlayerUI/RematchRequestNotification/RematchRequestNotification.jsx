a8963f9e93cc842b6c0c121b0d60a6a188e9db4c
Moamal-2000 <moamalalaapro1@gmail.com>
2026-01-17 23:16:50 +0300

    feat(intl): implement translation for RematchRequestNotification content in all current locals

--- src/components/XOGame/MultiPlayerUI/RematchRequestNotification/RematchRequestNotification.jsx
+++ src/components/XOGame/MultiPlayerUI/RematchRequestNotification/RematchRequestNotification.jsx
@@ -2,12 +2,14 @@
 
 import { socket } from "@/socket/socket";
 import { useMultiplayerStore } from "@/stores/multiplayer.store/multiplayer.store";
+import { useTranslations } from "next-intl";
 import { useEffect, useState } from "react";
 import s from "./RematchRequestNotification.module.scss";
 
 const RematchRequestNotification = () => {
   const { updateMultiplayerState, rematchRequest } = useMultiplayerStore();
   const [isVisible, setIsVisible] = useState(false);
+  const t = useTranslations("modals.rematch_request_notification");
 
   useEffect(() => {
     socket.on("rematch-request-received", () => {
@@ -37,12 +39,12 @@ const RematchRequestNotification = () => {
   return (
     <div className={s.overlay}>
       <div className={s.notification}>
-        <h2>Rematch Request</h2>
-        <p>Your opponent wants to play another round!</p>
+        <h2>{t("title")}</h2>
+        <p>{t("description")}</p>
 
         <div className={s.buttons}>
-          <button onClick={handleAccept}>Accept</button>
-          <button onClick={handleReject}>Decline</button>
+          <button onClick={handleAccept}>{t("accept")}</button>
+          <button onClick={handleReject}>{t("decline")}</button>
         </div>
       </div>
     </div>
