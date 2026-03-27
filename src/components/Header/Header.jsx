fbda2d874d3f77d5973373bfab9b261d7b5b176b
Moamal-2000 <moamalalaapro1@gmail.com>
2026-03-27 10:15:41 +0300

    accessibility(Header): add translated title and aria-label for logo link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,23 @@
 import { Link } from "@/i18n/navigation";
+import { useTranslations } from "next-intl";
 import GitHubButton from "../Shared/Buttons/GitHubButton/GitHubButton";
 import s from "./Header.module.scss";
 import HeaderButtons from "./HeaderButtons/HeaderButtons";
 import Logo from "./Logo/Logo";
 
 const Header = () => {
+  const t = useTranslations("header");
+  const logoTitle = t("logo_home");
+
   return (
     <header className={s.header}>
       <div className={s.wrapper}>
-        <Link href="/" className={s.logo}>
+        <Link
+          href="/"
+          className={s.logo}
+          title={logoTitle}
+          aria-label={logoTitle}
+        >
           <Logo />
         </Link>
         <GitHubButton />
